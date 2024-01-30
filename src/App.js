59344ea90e48fe764a12e6d1767fe7caca097812
saeedshekari <mohammadsaeed.shekari1373@gmail.com>
2024-01-30 21:18:02 +0330

    sending price as a number not string

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ function App() {
           <hr/>
         <h1 className='section2'>using props for products</h1>
         <div className='box-container'>
-            <Box text="USB" src='img3.jpg' price="2000$" explain= "best and cheapest product"/>
-            <Box text="AUX" src='./images/img2.jpg' price="100$" explain= "for car audio sys"/>
-            <Box text="Charger" src='./images/img1.jpg' price="300$" explain= "for mobile"/>
+            <Box text="USB" src='img3.jpg' price={2000} explain= "best and cheapest product"/>
+            <Box text="AUX" src='./images/img2.jpg' price={100} explain= "for car audio sys"/>
+            <Box text="Charger" src='./images/img1.jpg' price={300} explain= "for mobile"/>
         </div>
     </div>
   )
--- src/Box/Box.js
+++ src/Box/Box.js
@@ -7,7 +7,7 @@ export default function Box(props) {
         <div id='box-container'>
             <h2 className='text'>{props.text}</h2>
             <img src={props.src} alt='pic' id='pict'/>
-            <p className='price'>{props.price}</p>
+            <p className='price'>{props.price} $</p>
             <p className='explain'>{props.explain}</p>
             <button className='box-btn'>Add to cart</button>
         </div>
