6eabfaa780025683b0aa98d9bdc16154e9412e10
saeedshekari <mohammadsaeed.shekari1373@gmail.com>
2024-01-31 16:38:13 +0330

    using spred syntax in sending props to the Box component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css'
 import Product from './Product/Product';
 import Box from './Box/Box';
 function App() {
+  let allProducts = [
+    {id:1 , text: "AUX", src: './images/img2.jpg', price: 100, explain: "for car audio sys", count: 6},
+    {id:2 , text: "USB", src: 'img3.jpg', price: 2000, explain: "best and cheapest product", count: 12},
+    {id:3 , text: "Charger", src: './images/img1.jpg', price: 300, explain: "for mobile", count: 0}
+  ]
   return (
     <div>
         <div id='container'>
@@ -15,10 +20,17 @@ function App() {
           <hr/>
         <h1 className='section2'>using props for products</h1>
         <div className='box-container'>
-            <Box text="AUX" src='./images/img2.jpg' price={100} explain= "for car audio sys" count={6}/>
+            {
+            /* <Box text="AUX" src='./images/img2.jpg' price={100} explain= "for car audio sys" count={6}/>
             <Box text="USB" src='img3.jpg' price={2000} explain= "best and cheapest product" count={12}/>
             <Box text="Charger" src='./images/img1.jpg' price={300} explain= "for mobile" count={0}/>
-        </div>
+          */}
+ {/* or */}
+
+            <Box {...allProducts[0]}/>
+            <Box {...allProducts[1]}/>
+            <Box {...allProducts[2]}/>
+          </div>
     </div>
   )
 }
