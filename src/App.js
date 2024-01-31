24a14317edd58607d15ecf53698a4ccba6b31781
saeedshekari <mohammadsaeed.shekari1373@gmail.com>
2024-01-31 15:44:00 +0330

    using conditional props

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ function App() {
           <hr/>
         <h1 className='section2'>using props for products</h1>
         <div className='box-container'>
-            <Box text="USB" src='img3.jpg' price={2000} explain= "best and cheapest product"/>
-            <Box text="AUX" src='./images/img2.jpg' price={100} explain= "for car audio sys"/>
-            <Box text="Charger" src='./images/img1.jpg' price={300} explain= "for mobile"/>
+            <Box text="AUX" src='./images/img2.jpg' price={100} explain= "for car audio sys" count={6}/>
+            <Box text="USB" src='img3.jpg' price={2000} explain= "best and cheapest product" count={12}/>
+            <Box text="Charger" src='./images/img1.jpg' price={300} explain= "for mobile" count={0}/>
         </div>
     </div>
   )
--- src/Box/Box.js
+++ src/Box/Box.js
@@ -2,7 +2,27 @@ import React from 'react'
 import './Box.css'
 
 export default function Box(props) {
-  return (
+//   if(props.price>1000){
+//   return (
+    
+//         <div id='box-container'>
+//             <h2 className='text'>{props.text}</h2>
+//             <img src={props.src} alt='pic' id='pict'/>
+//             <p className='price'>{props.price} $</p>
+//             <p className='explain'>{props.explain}</p>
+//             <button className='box-btn'>Add to cart</button>
+//         </div>
+//   )
+// }else{
+//   return null
+// }
+
+
+//or
+
+
+return (
+    props.price >200 && props.count &&
     
         <div id='box-container'>
             <h2 className='text'>{props.text}</h2>
@@ -13,4 +33,6 @@ export default function Box(props) {
         </div>
     
   )
+
+
 }
