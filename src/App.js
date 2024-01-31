8b394a93f2db86bd1cbd570cf09a11f1dd7991bb
saeedshekari <mohammadsaeed.shekari1373@gmail.com>
2024-01-31 18:31:43 +0330

    creating new branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
+import React from 'react'
+import Clients from './Users/Users';
+import './App.css'
+import Product from './Product/Product';
+import Box from './Box/Box';
+let allProducts = [
+  {text: "AUX", src: './images/img2.jpg', price: 100, explain: "for car audio sys", count: 6},
+  {text: "USB", src: 'img3.jpg', price: 2000, explain: "best and cheapest product", count: 12},
+  {text: "Charger", src: './images/img1.jpg', price: 300, explain: "for mobile", count: 0}
+]
+
 function App() {
   return (
     <div>
+        <div id='container'>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Clients/>
+        </div>
+          <hr/>
+        <h1 className='section2'>using props for products</h1>
+        <div className='box-container'>
+            {
+            /* <Box text="AUX" src='./images/img2.jpg' price={100} explain= "for car audio sys" count={6}/>
+            <Box text="USB" src='img3.jpg' price={2000} explain= "best and cheapest product" count={12}/>
+            <Box text="Charger" src='./images/img1.jpg' price={300} explain= "for mobile" count={0}/>
+          */}
+ {/* or */}
+
+            <Box {...allProducts[0]}/>
+            <Box {...allProducts[1]}/>
+            <Box {...allProducts[2]}/>
+          </div>
     </div>
   )
 }
---
+++ src/Box/Box.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import './Box.css'
+
+// export default function Box(props) {
+// //   if(props.price>1000){
+// //   return (
+    
+// //         <div id='box-container'>
+// //             <h2 className='text'>{props.text}</h2>
+// //             <img src={props.src} alt='pic' id='pict'/>
+// //             <p className='price'>{props.price} $</p>
+// //             <p className='explain'>{props.explain}</p>
+// //             <button className='box-btn'>Add to cart</button>
+// //         </div>
+// //   )
+// // }else{
+// //   return null
+// // }
+// //or
+
+// return (
+//     props.price >200 && props.count &&
+    
+//         <div id='box-container'>
+//             <h2 className='text'>{props.text}</h2>
+//             <img src={props.src} alt='pic' id='pict'/>
+//             <p className='price'>{props.price} $</p>
+//             <p className='explain'>{props.explain}</p>
+//             <button className='box-btn'>Add to cart</button>
+//         </div>
+    
+//   )
+// }
+
+//or
+export default function Box({text: productText,src,price,explain,count}) {  
+  let text = 'introduction to products'
+  return (
+      price >200 && count &&
+          <div id='box-container'>
+              <h2 className='text'>{productText}</h2>
+              <img src={src} alt='pic' id='pict'/>
+              <p className='price'>{price} $</p>
+              <p className='explain'>{explain}</p>
+              <button className='box-btn'>Add to cart</button>
+              <p>{text}</p>
+          </div>
+      
+    )
+  }
+  
+
