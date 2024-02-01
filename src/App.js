0a93cc77ddaa14fc77dc780a39cd5a76dd289ffb
saeedshekari <mohammadsaeed.shekari1373@gmail.com>
2024-02-01 22:52:57 +0330

    using setState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-function App() {
-  return (
-    <div>
-    </div>
-  )
-}
+import React, { Component } from 'react'
+import Users from './Users/Users'
 
-export default App;
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      userInfo: [
+        {id:1, name: 'ali', score:17.5,},
+        {id:2, name: 'hamed', score:18.5,},
+        {id:3, name: 'ahmad', score:16.3,},
+      ]
+    }
+    setTimeout(()=>{  
+      this.setState({   // rerender state after 3 seconds with new values
+        userInfo:[
+          {id:1, name: 'sajad', score:17.5,},
+          {id:2, name: 'milad', score:18.5,},
+          {id:3, name: 'morteza', score:16.3,},
+        ]
+      })
+    },3000)
+  }
+  keyDownHandler(){
+    console.log('sth is typed');
+  }
+  clickHandler(){
+    alert('you clicked on the button')
+  }
+  render() {
+    return (
+      <div>
+        <Users {...this.state.userInfo[0]}></Users>
+        <Users {...this.state.userInfo[1]}></Users>
+        <Users {...this.state.userInfo[2]}></Users>
+        <input type='text' onKeyDown = {this.keyDownHandler}/>
+        <button onClick = {this.clickHandler}>click on me</button>
+        
+
+      </div>
+    )
+  }
+}
---
+++ src/Users/Users.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Users(props) {
+    
+  return (
+    <div>
+        <li>{props.name}</li>
+    </div>
+  )
+}
