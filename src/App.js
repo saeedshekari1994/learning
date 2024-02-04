a19e52b1b98fb2c6f6ec26e30f039c80fcea14ee
saeedshekari <mohammadsaeed.shekari1373@gmail.com>
2024-02-04 17:22:11 +0330

    writing notifCounter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import ConditionalRendering from './Components/ConditionalRendering/ConditionalRendering'
+import NotifCount from './Components/NotifCount/NotifCount'
 export default class App extends Component {
   render() {
     return (
       <div>
+        {/* session 62 */}
         <ConditionalRendering></ConditionalRendering>
+
+        {/* session 63 and 64 */}
+        <NotifCount>
+          
+        </NotifCount>
       </div>
     )
   }
