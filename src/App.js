bdd19984b18fc084238e5abcd5e9703c5f8b3500
saeedshekari <mohammadsaeed.shekari1373@gmail.com>
2024-02-18 12:59:43 +0330

    todo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-function App() {
-  return (
-    <div>
-    </div>
-  )
-}
+import React, { Component } from 'react'
+import TodoList from './components/TodoList/TodoList'
+import './App.css'
 
-export default App;
+export default class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <TodoList></TodoList>
+            </div>
+        )
+    }
+}
