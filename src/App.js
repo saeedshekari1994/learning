13f102f9773969aaba3c6fc7f79196250afa9d6f
saeedshekari <mohammadsaeed.shekari1373@gmail.com>
2024-03-24 11:01:47 +0330

    fake api + table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-function App() {
-  return (
-    <div>
-    </div>
-  )
-}
+import React, { Component } from 'react'
+import {Table , Container , Alert} from 'react-bootstrap'
 
-export default App;
+export default class App extends Component {
+  constructor(){
+      super()
+      this.state = {
+          posts:[]
+      }
+  }
+  componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/posts').then(response=>response.json())
+      .then(data=>{
+          this.setState({
+              posts : data
+          })
+      })
+  }
+  render() {
+      return (
+          <Container className='mt-5'>
+              {this.state.posts.length > 0 ? (
+                              <Table striped bordered hover variant="dark">
+                                  <thead>
+                                      <tr>
+                                          <th>ID</th>
+                                          <th>Title</th>
+                                          <th>Body</th>  
+                                      </tr>
+                                  </thead>
+                                  <tbody>
+                                      {this.state.posts.map(post=>{
+                                          return(
+                                              <tr>
+                                                  <td>{post.id}</td>
+                                                  <td>{post.title}</td>
+                                                  <td>{post.body}</td>
+                                              </tr>
+                                          )
+                                      })}
+                                  </tbody>
+                              </Table>
+              
+              ): 
+              (
+                  <Alert variant='info'>loadig...</Alert>
+
+              )}
+          </Container>
+  )
+  }            
+}
